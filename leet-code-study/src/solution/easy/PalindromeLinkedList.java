79d711c1e6c4c46159be62845fb34c15b6f8999b
kkan0802 <kkan0802@gmail.com>
2022-05-25 14:03:05 +0900

    234. Palindrome Linked List solved

---
+++ leet-code-study/src/solution/easy/PalindromeLinkedList.java
@@ -0,0 +1,34 @@
+package solution.medium;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+import baseClass.ListNode;
+
+public class PalindromeLinkedList {
+
+    /*
+     * 234. Palindrome Linked List
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        ListNode startNode = head;
+        while (startNode != null) {
+            stack.push(startNode.val);
+            startNode = startNode.next;
+        }
+
+        while (head != null) {
+            if (stack.pop() != head.val) return false;
+            head = head.next;
+        }
+
+        if (!stack.isEmpty()) return false;
+
+        return true;
+    }
+
+}
